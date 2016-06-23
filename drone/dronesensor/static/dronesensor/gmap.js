cd7e346ffa2af633819596f2d7bb7f03c73d66e9
ktinubu <uwarayo@gmail.com>
2016-06-23 12:38:40 -0400

    awsf

--- drone/dronesensor/static/dronesensor/gmap.js
+++ drone/dronesensor/static/dronesensor/gmap.js
@@ -4,4 +4,20 @@ function initMap() {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
+  var location1 = {lat: -34.397, lng: 150.644}
+  var marker = new google.maps.Marker ({
+    position: location1,
+    map: map,
+    title: 'lol'
+    // get information get co-ordinates 
+    var temepraturePoint {
+      gpsPosition: 
+      teperature: 
+    }
+
+    //loop 
+      //temepraturePoint.gpsPosition = shadow.get()
+      // temepraturePoint.teperature = shadow.get()
+    // or use latLong
+  })
 }
